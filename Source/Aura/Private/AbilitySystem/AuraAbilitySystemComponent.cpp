d3b6c2e57d9ef07ff3f2ea954c43cf6d278956fd
husteve07 <77590544+husteve07@users.noreply.github.com>
2023-11-11 19:25:59 -0500

    0.7.2 Bind Delegate Dependency with Overlay Widget Controller

--- Source/Aura/Private/AbilitySystem/AuraAbilitySystemComponent.cpp
+++ Source/Aura/Private/AbilitySystem/AuraAbilitySystemComponent.cpp
@@ -12,5 +12,10 @@ void UAuraAbilitySystemComponent::AbilityActorSet()
 void UAuraAbilitySystemComponent::EffectApplied(UAbilitySystemComponent* AbilitySystemComponent,
                                                 const FGameplayEffectSpec& EffectSpec, FActiveGameplayEffectHandle ActiveEffectHandle)
 {
-	GEngine->AddOnScreenDebugMessage(1, 8.f, FColor::Blue, FString("Effect Applied"));
+	FGameplayTagContainer TagContainer;
+	EffectSpec.GetAllAssetTags(TagContainer);
+
+	EffectAssetTags.Broadcast(TagContainer);
+	
+
 }
