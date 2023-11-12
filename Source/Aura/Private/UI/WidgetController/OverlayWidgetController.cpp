cd474dcc7769f0bd89081ea522d551a09235f986
husteve07 <77590544+husteve07@users.noreply.github.com>
2023-11-11 21:39:17 -0500

    0.7.3 Overlay Widget controller Callback, dependency with declared data row table struct

--- Source/Aura/Private/UI/WidgetController/OverlayWidgetController.cpp
+++ Source/Aura/Private/UI/WidgetController/OverlayWidgetController.cpp
@@ -26,12 +26,16 @@ void UOverlayWidgetController::BindCallbacksToDependencies()
 	AbilitySystemComponent->GetGameplayAttributeValueChangeDelegate(AuraAttributeSet->GetMaxManaAttribute()).AddUObject(this, &UOverlayWidgetController::MaxManaChanged);
 
 	Cast<UAuraAbilitySystemComponent>(AbilitySystemComponent)->EffectAssetTags.AddLambda(
-		[](const FGameplayTagContainer& AssetTags)
+		[this](const FGameplayTagContainer& AssetTags)
 		{
 			for(const FGameplayTag& Tag : AssetTags)
 			{
-				const FString MSG = FString::Printf(TEXT(" Widget Controller: GE Tag: %s"), *Tag.ToString());
-				GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Green, MSG);
+				FGameplayTag Message = FGameplayTag::RequestGameplayTag(FName("Message"));
+				if(Tag.MatchesTag(Message))
+				{
+					const FUIWidgetRow* Row = GetDataRowByTag<FUIWidgetRow>(MessageWidgetDataTable, Tag);
+					MessageWidgetRowDelegate.Broadcast(*Row);
+				}
 			}
 		}
 	);
