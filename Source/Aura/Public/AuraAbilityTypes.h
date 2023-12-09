59da7c69061241a9ef1a35520bde7bd25f629d67
husteve07 <77590544+husteve07@users.noreply.github.com>
2023-12-08 21:08:04 -0500

    1.3.10 Implement Struct Op Types

--- Source/Aura/Public/AuraAbilityTypes.h
+++ Source/Aura/Public/AuraAbilityTypes.h
@@ -10,7 +10,7 @@ struct FAuraGameplayEffectContext : public FGameplayEffectContext
 public:
 
 	bool IsCriticalHit() const {return bIsCriticalHit;}
-	bool IsBlockedHit() const {return bIsBlockedHit();}
+	bool IsBlockedHit() const {return bIsBlockedHit;}
 
 	void SetIsCriticalHit(bool bInIsCriticalHit){bIsCriticalHit = bInIsCriticalHit;}
 	void SetIsBlockedHit(bool bInIsBlockedHit){bIsBlockedHit = bInIsBlockedHit;}
@@ -18,7 +18,20 @@ public:
 	/** Returns the actual struct used for serialization, subclasses must override this! */
 	virtual UScriptStruct* GetScriptStruct() const
 	{
-		return FGameplayEffectContext::StaticStruct();
+		return StaticStruct();
+	}
+
+	/** Creates a copy of this context, used to duplicate for later modifications */
+	virtual FAuraGameplayEffectContext* Duplicate() const
+	{
+		FAuraGameplayEffectContext* NewContext = new FAuraGameplayEffectContext();
+		*NewContext = *this;
+		if (GetHitResult())
+		{
+			// Does a deep copy of the hit result
+			NewContext->AddHitResult(*GetHitResult(), true);
+		}
+		return NewContext;
 	}
 
 	virtual bool NetSerialize(FArchive& Ar, class UPackageMap* Map, bool& bOutSuccess);
@@ -29,6 +42,14 @@ protected:
 	bool bIsCriticalHit = false;
 	UPROPERTY()
 	bool bIsBlockedHit = false;
-
 	
+};
+template<>
+struct TStructOpsTypeTraits<FAuraGameplayEffectContext> : public TStructOpsTypeTraitsBase2<FAuraGameplayEffectContext>
+{
+	enum
+	{
+		WithNetSerializer = true,
+		WithCopy = true
+	};
 };
